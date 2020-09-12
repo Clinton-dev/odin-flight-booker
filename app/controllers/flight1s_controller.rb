30ec9deaf3f07df88ad3dfa112020f217ee5608a
clinton wambugu <clintonwambugu@gmail.com>
2020-09-12 20:25:32 +0300

    Create Search form on screen one

--- app/controllers/flight1s_controller.rb
+++ app/controllers/flight1s_controller.rb
@@ -1,5 +1,8 @@
 class Flight1sController < ApplicationController
   def index
+    @airports = Airport.all
+    @airport_options = @airports.map { |a| [a.code, a.id] }
+    @flights = Flight1.all
   end
 
   def new

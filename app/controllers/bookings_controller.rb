cfcff7ac490eb0ff2ab8ac85eac6d7dbec69a487
clinton wambugu <clintonwambugu@gmail.com>
2020-09-14 23:16:08 +0300

    Setup create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,27 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @flight = Flight1.find(params[:flight][:flight_id])
   end
+
+  def create
+    @booking = Booking.new(flight_id: params[:flight_id].to_i)
+
+    passengers_params[:passengers_attributes].each do |passenger|
+      @booking.passengers.build(passenger[1]) 
+    end
+
+    @booking.save
+
+    redirect_to booking_path(@booking)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @flight = @booking.flight
+  end
+
+  private
+
+  def passengers_params
+    params.require(:booking).permit(passengers_attributes: [:name, :email])
+  end
 end
